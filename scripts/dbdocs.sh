f6c572ad46cdc5bc3bb9610a73b187b3988eb91b
Harison Gachuru <harisongachuru@gmail.com>
2021-06-29 14:30:17 +0300

    Create utility scripts for generating database documentation

---
+++ scripts/dbdocs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+export BASE_DIR=.
+export DB_DOCS_DIR="$BASE_DIR"/docs/db/
+migrations_sql_file="$DB_DOCS_DIR"/migrations.sql
+migrations_dbml_file="$DB_DOCS_DIR"/migrations.dbml
+
+# install dbdocs and dbml cli
+# https://www.dbml.org/cli/
+# npm install -g @dbml/cli
+# npm install -g dbdocs
+
+# login to dbdocs
+# https://dbdocs.io/docs
+# dbdocs login 
+
+# clean the migrations SQL file
+comment="s/^/--/g"
+delete="d"
+sed "/BEGIN/${delete};/COMMIT/${delete};" -i "$migrations_sql_file"
+sed -e "/CREATE INDEX/${comment};/DROP COLUMN/${comment};" -i "$migrations_sql_file"
+
+# generate dbml from SQL
+sql2dbml "$migrations_sql_file" -o "$migrations_dbml_file"
+
+# generate documentation
+dbdocs build "$migrations_dbml_file" --project="church-ims"
+
+# clean up
+rm -rf "$DB_DOCS_DIR"
