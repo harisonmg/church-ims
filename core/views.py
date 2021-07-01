e8b06f62189f15cca7136902dd5d748733a2bcad
Harison Gachuru <harisongachuru@gmail.com>
2021-07-02 02:39:36 +0300

    Update core app

--- core/views.py
+++ core/views.py
@@ -1,11 +1,10 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
-
-from children.views import ChildrenByUserListView
 
 
 class IndexView(TemplateView):
     template_name = "core/index.html"
 
 
-class DashboardView(ChildrenByUserListView):
+class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = "core/dashboard.html"
