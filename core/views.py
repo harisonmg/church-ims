f2b4934ed2e06ca80f58bc69f6f5c246cff1eae7
Harison Gachuru <harisongachuru@gmail.com>
2021-07-01 11:09:45 +0300

    Update core URLconf and views

--- core/views.py
+++ core/views.py
@@ -1,5 +1,3 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
 from django.views.generic import TemplateView
 
 from children.views import ChildrenByUserListView
