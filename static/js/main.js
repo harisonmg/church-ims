30fd478b3eff421ab5d13193ead3efb2052c7b8e
Harison Gachuru <harisongachuru@gmail.com>
2021-07-03 15:36:16 +0300

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,35 +1,32 @@
-const currentPagePath = document.getElementById("currentPagePath")
-const currentPageUrl = currentPagePath.getAttribute("value")
+const currentPagePath = document.getElementById("currentPagePath");
+const currentPageUrl = currentPagePath.getAttribute("value");
 
-let addActiveClass = function(nav){
-    document.addEventListener("DOMContentLoaded", function() {
-        let navigationLinks = nav.getElementsByClassName("nav-link")
-        for (var i=0; i < navigationLinks.length; i++){
-            if (navigationLinks[i].getAttribute("href").includes(currentPageUrl)) {
-                navigationLinks[i].classList.add("active");
-                navigationLinks[i].classList.remove("text-white");
-                navigationLinks[i].setAttribute("aria-current", "page");
-            }
-        }
-    })
+let navLinkAddActiveClass = function(navLink){
+    navLink.classList.add("active");
+    navLink.classList.remove("text-white");
+    navLink.setAttribute("aria-current", "page");
 };
 
-let addTextWhite = function(nav){
+let navigationAddActiveClass = function(nav){
     document.addEventListener("DOMContentLoaded", function() {
         let navigationLinks = nav.getElementsByClassName("nav-link")
         for (var i=0; i < navigationLinks.length; i++){
-            if (!navigationLinks[i].classList.contains("active")) {
-                navigationLinks[i].classList.add("text-white")
-            }
-        }
-    })
-}
+            let navLink = navigationLinks[i];
+            if (navLink.classList.contains("dropdown-toggle")) {
+                let navDropDownLinks = navLink.getElementsByClassName("dropdown-item");
+                for (var j=0; j < navDropDownLinks.length; j++){
+                    if (currentPageUrl.includes(navDropDownLinks[j].getAttribute("href"))){
+                        navLinkAddActiveClass(navLink);
+                    }
+                };
+            } else {
+                if (currentPageUrl.includes(navLink.getAttribute("href"))) {
+                    navLinkAddActiveClass(navLink);
+                };
+            };
+        };
+    });
+};
 
 let mainNavigation = document.getElementById("mainNavigation");
-let navTabs = document.getElementsByClassName("nav-tabs");
-addActiveClass(mainNavigation);
-
-for (i=0; i < navTabs.length; i++){
-    addActiveClass(navTabs[i]);
-    addTextWhite(navTabs[i]);
-}
+navigationAddActiveClass(mainNavigation);
