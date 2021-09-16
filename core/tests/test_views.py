b7cc5b43822323fcf32e9f59a87bb5ffe1ab1a12
Harison Gachuru <harisongachuru@gmail.com>
2021-09-16 18:19:37 +0300

    Add dashboard view

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -1,5 +1,9 @@
-from django.test import RequestFactory, SimpleTestCase
+from django.contrib.auth.models import AnonymousUser
+from django.test import RequestFactory, SimpleTestCase, TestCase
 
+from faker import Faker
+
+from accounts.factories import UserFactory
 from core import views
 
 
@@ -17,3 +21,34 @@ class IndexViewTestCase(SimpleTestCase):
         response = self.view.as_view()(self.request)
         with self.assertTemplateUsed("core/index.html"):
             response.render()
+
+
+class DashboardViewTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        cls.url = "/dashboard/"
+        cls.view = views.DashboardView
+
+    def test_template_used(self):
+        factory = RequestFactory()
+        request = factory.get("dummy_path/")
+        request.user = AnonymousUser
+
+        response = self.view.as_view()(request)
+        with self.assertTemplateUsed("core/dashboard.html"):
+            response.render()
+
+    def test_view_requires_login(self):
+        response = self.client.get(self.url)
+        self.assertRedirects(response, f"/accounts/login/?next={self.url}")
+
+    def test_logged_in_response_status_code(self):
+        fake = Faker()
+        user_password = fake.password()
+        user = UserFactory(password=user_password)
+
+        self.client.login(email=user.email, password=user_password)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
--- core/views.py
+++ core/views.py
@@ -1,5 +1,10 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
 
 class IndexView(TemplateView):
     template_name = "core/index.html"
+
+
+class DashboardView(LoginRequiredMixin, TemplateView):
+    template_name = "core/dashboard.html"
