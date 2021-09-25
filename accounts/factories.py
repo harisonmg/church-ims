107e3bc0c6b54ba84a3d866f747c4e0412c60dad
Harison Gachuru <harisongachuru@gmail.com>
2021-09-25 23:52:37 +0300

    Update user factory to add groups and permissions, create group factory

--- accounts/factories.py
+++ accounts/factories.py
@@ -1,16 +1,32 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 
-from factory.django import DjangoModelFactory
-from factory.faker import Faker
+import factory
 
 
-class UserFactory(DjangoModelFactory):
+class GroupFactory(factory.django.DjangoModelFactory):
+    class Meta:  # noqa
+        model = Group
+
+    name = factory.Sequence(lambda n: f"Group {n}")
+
+    @factory.post_generation
+    def permissions(self, create, extracted):
+        if not create:
+            return
+
+        if extracted:
+            for permission in extracted:
+                self.permissions.add(permission)
+
+
+class UserFactory(factory.django.DjangoModelFactory):
     class Meta:  # noqa
         model = get_user_model()
 
-    username = Faker("user_name")
-    email = Faker("email")
-    password = Faker("password")
+    username = factory.Faker("user_name")
+    email = factory.Faker("email")
+    password = factory.Faker("password")
 
     @classmethod
     def _create(cls, model_class, *args, **kwargs):
@@ -19,6 +35,24 @@ class UserFactory(DjangoModelFactory):
         """
         manager = cls._get_manager(model_class)
         return manager.create_user(*args, **kwargs)
+
+    @factory.post_generation
+    def groups(self, create, extracted):
+        if not create:
+            return
+
+        if extracted:
+            for group in extracted:
+                self.groups.add(group)
+
+    @factory.post_generation
+    def user_permissions(self, create, extracted):
+        if not create:
+            return
+
+        if extracted:
+            for permission in extracted:
+                self.user_permissions.add(permission)
 
 
 class AdminUserFactory(UserFactory):
--- accounts/tests/test_factories.py
+++ accounts/tests/test_factories.py
@@ -1,9 +1,27 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group, Permission
 from django.test import TestCase
 
 from faker import Faker
 
 from accounts import factories
+
+
+class GroupFactoryTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+    def test_name(self):
+        group = factories.GroupFactory()
+        saved_group = Group.objects.first()
+        self.assertEqual(saved_group.name, group.name)
+
+    def test_permissions(self):
+        add_permissions = Permission.objects.filter(name__icontains="can add")
+        group = factories.GroupFactory(permissions=tuple(add_permissions))
+        saved_group = Group.objects.first()
+        self.assertEqual(saved_group.permissions, group.permissions)
 
 
 class UserFactoryTestCase(TestCase):
@@ -15,24 +33,52 @@ class UserFactoryTestCase(TestCase):
         cls.user_password = cls.fake.password()
         cls.user = factories.UserFactory(password=cls.user_password)
 
-    def test_object_creation(self):
-        self.assertEqual(get_user_model().objects.count(), 1)
-
-    def test_is_staff(self):
-        self.assertFalse(self.user.is_staff)
-
     def test_username(self):
-        username = self.user.username
-        self.assertGreaterEqual(len(username), 5)
+        saved_user = get_user_model().objects.first()
+        self.assertEqual(saved_user.username, self.user.username)
 
     def test_email(self):
-        email = self.user.email
-        self.assertGreaterEqual(len(email), 7)
-        self.assertIn("@", email)
+        saved_user = get_user_model().objects.first()
+        self.assertEqual(saved_user.email, self.user.email)
 
     def test_password(self):
-        password = self.user_password
-        self.assertEqual(len(password), 10)
+        saved_user = get_user_model().objects.first()
+        self.assertEqual(saved_user.password, self.user.password)
+        self.assertNotEqual(saved_user.password, self.user_password)
+
+    def test_is_superuser(self):
+        saved_user = get_user_model().objects.first()
+        self.assertFalse(saved_user.is_superuser)
+
+    def test_is_staff(self):
+        saved_user = get_user_model().objects.first()
+        self.assertFalse(saved_user.is_staff)
+
+    def test_is_active(self):
+        saved_user = get_user_model().objects.first()
+        self.assertTrue(saved_user.is_active)
+
+    def test_groups(self):
+        # get permissions
+        add_permissions = Permission.objects.filter(name__icontains="can add")
+        change_permissions = Permission.objects.filter(name__icontains="can change")
+        delete_permissions = Permission.objects.filter(name__icontains="can delete")
+        editor_permissions = list(add_permissions) + list(change_permissions)
+
+        # create groups
+        admins_group = factories.GroupFactory(permissions=tuple(delete_permissions))
+        editors_group = factories.GroupFactory(permissions=tuple(editor_permissions))
+
+        # create user
+        user = factories.UserFactory(groups=(admins_group, editors_group))
+        saved_user = get_user_model().objects.last()
+        self.assertEqual(saved_user.groups, user.groups)
+
+    def test_user_permissions(self):
+        change_permissions = Permission.objects.filter(name__icontains="can change")
+        user = factories.UserFactory(user_permissions=tuple(change_permissions))
+        saved_user = get_user_model().objects.last()
+        self.assertEqual(saved_user.user_permissions, user.user_permissions)
 
 
 class AdminUserFactoryTestCase(TestCase):
@@ -41,12 +87,10 @@ class AdminUserFactoryTestCase(TestCase):
         super().setUpClass()
         cls.user = factories.AdminUserFactory()
 
-    def test_user_is_staff(self):
-        staff_users = get_user_model().objects.filter(is_staff=True)
-        self.assertTrue(self.user.is_staff)
-        self.assertEqual(len(staff_users), 1)
+    def test_is_superuser(self):
+        saved_user = get_user_model().objects.first()
+        self.assertTrue(saved_user.is_superuser)
 
-    def test_user_is_superuser(self):
-        superusers = get_user_model().objects.filter(is_superuser=True)
-        self.assertTrue(self.user.is_superuser)
-        self.assertEqual(len(superusers), 1)
+    def test_is_staff(self):
+        saved_user = get_user_model().objects.first()
+        self.assertTrue(saved_user.is_staff)
