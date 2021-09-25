f7e9e715c8bd88db9328d4cbb5b52e71450094c0
Harison Gachuru <harisongachuru@gmail.com>
2021-09-25 17:32:48 +0300

    Update PeopleListView to use the Person model

--- people/tests/test_views.py
+++ people/tests/test_views.py
@@ -1,10 +1,8 @@
-from django.contrib.auth.models import AnonymousUser
-from django.test import RequestFactory, TestCase
-
-from faker import Faker
+from django.test import TestCase
 
 from accounts.factories import UserFactory
-from people import views
+from people.factories import PersonFactory
+from people.models import Person
 
 
 class PeopleListViewTestCase(TestCase):
@@ -13,26 +11,62 @@ class PeopleListViewTestCase(TestCase):
         super().setUpClass()
 
         cls.url = "/people/"
-        cls.view = views.PeopleListView
+        cls.user = UserFactory()
+        cls.table_head = """
+        <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Username</th>
+              <th scope="col">Full name</th>
+            </tr>
+        </thead>
+        """
 
-    def test_template_used(self):
-        factory = RequestFactory()
-        request = factory.get("dummy_path/")
-        request.user = AnonymousUser
-
-        response = self.view.as_view()(request)
-        with self.assertTemplateUsed("people/people_list.html"):
-            response.render()
-
-    def test_view_requires_login(self):
+    def test_anonymous_user_response(self):
         response = self.client.get(self.url)
         self.assertRedirects(response, f"/accounts/login/?next={self.url}")
 
-    def test_logged_in_response_status_code(self):
-        fake = Faker()
-        user_password = fake.password()
-        user = UserFactory(password=user_password)
-
-        self.client.login(email=user.email, password=user_password)
+    def test_authenticated_user_response(self):
+        self.client.force_login(self.user)
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
+
+    def test_template_used(self):
+        self.client.force_login(self.user)
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(response, "people/people_list.html")
+
+    def test_context_data_contains_people(self):
+        self.client.force_login(self.user)
+        response = self.client.get(self.url)
+        self.assertIn("people", response.context)
+
+    def test_is_paginated(self):
+        PersonFactory.create_batch(11)
+        self.client.force_login(self.user)
+        response = self.client.get(self.url)
+        self.assertTrue(response.context.get("is_paginated"))
+        self.assertEqual(len(response.context.get("people")), 10)
+
+    def test_pagination_lists_all_items(self):
+        PersonFactory.create_batch(12)
+        self.client.force_login(self.user)
+        response = self.client.get(self.url + "?page=2")
+        expected_people = list(Person.objects.all())[-2:]
+        people = list(response.context.get("people"))
+        self.assertEqual(people, expected_people)
+
+    def test_response_with_no_people(self):
+        self.client.force_login(self.user)
+        response = self.client.get(self.url)
+        with self.assertRaises(AssertionError):
+            self.assertInHTML(self.table_head, response.content.decode())
+        self.assertInHTML("There are no people yet!", response.content.decode())
+
+    def test_response_with_people(self):
+        PersonFactory.create_batch(3)
+        self.client.force_login(self.user)
+        response = self.client.get(self.url)
+        with self.assertRaises(AssertionError):
+            self.assertInHTML("There are no people yet!", response.content.decode())
+        self.assertInHTML(self.table_head, response.content.decode())
--- people/views.py
+++ people/views.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+
+from .models import Person
 
 
-class PeopleListView(LoginRequiredMixin, TemplateView):
+class PeopleListView(LoginRequiredMixin, ListView):
+    context_object_name = "people"
+    model = Person
+    paginate_by = 10
     template_name = "people/people_list.html"
