26b15f277d228715b13ff2bc24d5cf7ae9953162
Harison Gachuru <harisongachuru@gmail.com>
2021-09-27 23:33:40 +0300

    Partially refactor people list page FT using page object

--- functional_tests/pages.py
+++ functional_tests/pages.py
@@ -1,9 +1,14 @@
+from selenium.common.exceptions import NoSuchElementException
+
+
 class LoginPage:
     def __init__(self, test):
         self.test = test
 
     def get_attributes(self):
-        self.test.assertEqual(self.test.browser.title, self.test.get_site_name())
+        self.title = self.test.browser.title
+        self.test.assertEqual(self.title, self.test.get_site_name())
+
         self.h1 = self.test.browser.find_element_by_css_selector("h1").text
         self.test.assertEqual(self.h1, "Log in")
 
@@ -47,3 +52,63 @@ class LoginPage:
         self.email_input.send_keys(email)
         self.password_input.send_keys(password)
         self.login_button.click()
+
+
+class PeopleListPage:
+    def __init__(self, test):
+        self.test = test
+
+    def get_attributes(self):
+        self.title = self.test.browser.title
+        self.test.assertEqual(self.title, self.test.get_site_name())
+
+        self.h1 = self.test.browser.find_element_by_tag_name("h1").text
+        self.test.assertEqual(self.h1, "People")
+
+        # sidebar
+        self.sidebar_navigation = self.test.browser.find_element_by_id("sidebarMenu")
+        people_link = self.sidebar_navigation.find_element_by_link_text("People")
+        self.test.assertIn("active", people_link.get_attribute("class"))
+
+        # people list
+        self._get_people_list()
+
+        # search form
+        self._get_search_form()
+
+        # pagination
+        return self
+
+    def _get_people_list(self):
+        try:
+            self.people_table = self.test.browser.find_element_by_tag_name("table")
+            self.test.assertEqual(
+                self.people_table.find_element_by_tag_name("thead").text,
+                "# Username Full name",
+            )
+            self.people_list = self.people_table.find_elements_by_css_selector(
+                "tbody tr"
+            )
+        except NoSuchElementException:
+            pass
+
+    def _get_search_form(self):
+        try:
+            self.search_form = self.test.browser.find_element_by_id("search_form")
+            self.search_input = self.search_form.find_element_by_css_selector(
+                "input[type='search']"
+            )
+            self.test.assertEqual(
+                self.search_input.get_attribute("placeholder"), "Search"
+            )
+            self.search_button = self.search_form.find_element_by_css_selector(
+                "button[type='submit']"
+            )
+            self.test.assertEqual(self.search_button.text, "Search")
+        except NoSuchElementException:
+            pass
+
+    def search(self, search_term):
+        self.search_input.send_keys(search_term)
+        self.search_button.click()
+        return self.get_attributes()
--- functional_tests/records/test_people_list.py
+++ functional_tests/records/test_people_list.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import Permission
 
 from accounts.factories import UserFactory
 from functional_tests.base import FunctionalTestCase
-from functional_tests.pages import LoginPage
+from functional_tests.pages import LoginPage, PeopleListPage
 from people.factories import PersonFactory
 
 
@@ -25,113 +25,79 @@ class PeopleListTestCase(FunctionalTestCase):
         self.browser.get(self.live_server_url + "/accounts/login/")
         LoginPage(self).get_attributes().login(self.user.email, self.password)
 
-    def get_parent_attribute(self, element, attribute="class"):
-        return element.find_element_by_xpath("..").get_attribute(attribute)
-
-    def get_page_attributes(self):
-        self.assertEqual(self.browser.title, self.get_site_name())
-        self.assertEqual(self.browser.find_element_by_tag_name("h1").text, "People")
-
-        sidebar_navigation = self.browser.find_element_by_id("sidebarMenu")
-        people_link = sidebar_navigation.find_element_by_link_text("People")
-        self.assertIn("active", people_link.get_attribute("class"))
-
-    def get_page_navigation_links(self):
+    def get_pagination_links(self):
         page_navigation = self.browser.find_element_by_css_selector("nav .pagination")
-        page_navigation_links = page_navigation.find_elements_by_css_selector("li a")
-        return page_navigation_links
+        return page_navigation.find_elements_by_css_selector("li")
 
-    def get_people_list(self):
-        people_table = self.browser.find_element_by_tag_name("table")
-        self.assertEqual(
-            people_table.find_element_by_tag_name("thead").text, "# Username Full name"
-        )
-        people_list = people_table.find_elements_by_css_selector("tbody tr")
-        return people_list
-
-    def get_search_form(self):
-        search_form = self.browser.find_element_by_id("search_form")
-        search_input = search_form.find_element_by_css_selector("input[type='search']")
-        self.assertEqual(search_input.get_attribute("placeholder"), "Search")
-        search_button = search_form.find_element_by_css_selector(
-            "button[type='submit']"
-        )
-        self.assertEqual(search_button.text, "Search")
-        return search_input, search_button
+    def get_active_pagination_link(self):
+        return self.browser.find_element_by_css_selector(".pagination li.active a")
 
     def test_people_list(self):
         # An authorized user clicks on the people link on the sidebar
         self.login()
-        self.browser.find_element_by_link_text("People").click()
+        sidebar_navigation = self.browser.find_element_by_id("sidebarMenu")
+        sidebar_navigation.find_element_by_link_text("People").click()
 
-        # He is redirected to the people list page
+        # He is redirected to the people list page where he sees a list
+        # of people, a search form and a page navigation
         self.assertEqual(self.browser.current_url, self.live_server_url + "/people/")
-        self.get_page_attributes()
-
-        # He sees a list of people and a page navigation
-        people_list = self.get_people_list()
+        people_list_page = PeopleListPage(self).get_attributes()
         person = self.people[0]
-        self.assertEqual(people_list[0].text, f"1 {person.username} {person.full_name}")
-        self.assertEqual(len(people_list), 10)
+        self.assertEqual(
+            people_list_page.people_list[0].text,
+            f"1 {person.username} {person.full_name}",
+        )
+        self.assertEqual(len(people_list_page.people_list), 10)
 
-        page_navigation_links = self.get_page_navigation_links()
-        self.assertEqual(len(page_navigation_links), 6)
-        self.assertIn("disabled", self.get_parent_attribute(page_navigation_links[0]))
+        pagination_links = self.get_pagination_links()
+        self.assertEqual(len(pagination_links), 6)
+        self.assertIn("disabled", pagination_links[0].get_attribute("class"))
 
         # He clicks on the last link in the page navigation and is redirected
-        # to that page. The page also contains a list of people and a page navigation
-        page_navigation_links[-1].click()
+        # to that page. The page also contains a list of people, a search form
+        # and a page navigation
+        pagination_links[-1].click()
 
-        self.get_page_attributes()
-
-        people_list = self.get_people_list()
+        people_list_page = PeopleListPage(self).get_attributes()
         person = self.people[-2]
         self.assertEqual(
-            people_list[-2].text, f"4 {person.username} {person.full_name}"
+            people_list_page.people_list[-2].text,
+            f"4 {person.username} {person.full_name}",
         )
-        self.assertEqual(len(people_list), 5)
+        self.assertEqual(len(people_list_page.people_list), 5)
 
-        page_navigation_links = self.get_page_navigation_links()
-        self.assertEqual(len(page_navigation_links), 6)
+        pagination_links = self.get_pagination_links()
+        self.assertEqual(len(pagination_links), 6)
 
-        self.assertEqual(
-            page_navigation_links[-2].get_attribute("href"), self.browser.current_url
-        )
-        self.assertIn("active", self.get_parent_attribute(page_navigation_links[-2]))
-        self.assertIn("disabled", self.get_parent_attribute(page_navigation_links[-1]))
+        active_link = self.get_active_pagination_link()
+        self.assertEqual(active_link.get_attribute("href"), self.browser.current_url)
+        self.assertIn("disabled", pagination_links[-1].get_attribute("class"))
 
     def test_people_list_search(self):
-        # An authorized user visits the people list page
+        # An authorized user visits the people list page. He sees a list of people,
+        # a page navigation and a search form with placeholders
         self.login()
         self.browser.get(self.live_server_url + "/people/")
-        self.get_page_attributes()
+        people_list_page = PeopleListPage(self).get_attributes()
 
-        # He sees a list of people, a page navigation and a search form
-        # with placeholders
-        people_list = self.get_people_list()
         person = self.people[0]
-        self.assertEqual(people_list[0].text, f"1 {person.username} {person.full_name}")
-        self.assertEqual(len(people_list), 10)
+        self.assertEqual(
+            people_list_page.people_list[0].text,
+            f"1 {person.username} {person.full_name}",
+        )
+        self.assertEqual(len(people_list_page.people_list), 10)
 
-        self.get_search_form()
-
-        page_navigation_links = self.get_page_navigation_links()
-        self.assertEqual(len(page_navigation_links), 6)
-        self.assertIn("disabled", self.get_parent_attribute(page_navigation_links[0]))
+        pagination_links = self.get_pagination_links()
+        self.assertEqual(len(pagination_links), 6)
+        self.assertIn("disabled", pagination_links[0].get_attribute("class"))
 
         # He decides to search for a person that exists
-        search_input, search_button = self.get_search_form()
         person = self.people[20]
-        search_input.send_keys(person.full_name)
-        search_button.click()
-
-        people_list = self.get_people_list()
-        self.assertEqual(len(people_list), 1)
+        people_list_page = people_list_page.search(person.full_name)
+        self.assertEqual(len(people_list_page.people_list), 1)
 
         # He decides to search for a person that doesn't exist
-        search_input, search_button = self.get_search_form()
-        search_input.send_keys("Does not exist")
-        search_button.click()
+        people_list_page.search("Does not exist")
 
         self.assertEqual(
             self.browser.find_element_by_css_selector("p.lead").text,
