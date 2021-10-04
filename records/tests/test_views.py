25b7f8fb14eaf63adb027c6d497f72b1ec65466d
Harison Gachuru <harisongachuru@gmail.com>
2021-10-04 23:18:52 +0300

    Update TemperatureRecordsListView

--- records/tests/test_views.py
+++ records/tests/test_views.py
@@ -1,10 +1,9 @@
-from django.contrib.auth.models import AnonymousUser
-from django.test import RequestFactory, TestCase
-
-from faker import Faker
+from django.contrib.auth.models import Permission
+from django.test import TestCase
 
 from accounts.factories import UserFactory
-from records import views
+from records.factories import TemperatureRecordFactory
+from records.models import TemperatureRecord
 
 
 class TemperatureRecordsListViewTestCase(TestCase):
@@ -12,27 +11,82 @@ class TemperatureRecordsListViewTestCase(TestCase):
     def setUpClass(cls):
         super().setUpClass()
 
-        cls.url = "/dashboard/"
-        cls.view = views.TemperatureRecordsListView
+        cls.url = "/records/temperature/"
+        cls.table_head = """
+        <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Username</th>
+              <th scope="col">Temperature</th>
+              <th scope="col">Time</th>
+            </tr>
+        </thead>
+        """
+        # users
+        permission = Permission.objects.filter(name="Can view temperature record")
+        cls.user = UserFactory()
+        cls.authorized_user = UserFactory(user_permissions=tuple(permission))
+        cls.staff_user = UserFactory(is_staff=True)
 
-    def test_template_used(self):
-        factory = RequestFactory()
-        request = factory.get("dummy_path/")
-        request.user = AnonymousUser
-
-        response = self.view.as_view()(request)
-        with self.assertTemplateUsed("records/temperature_records_list.html"):
-            response.render()
-
-    def test_view_requires_login(self):
+    def test_anonymous_user_response(self):
         response = self.client.get(self.url)
         self.assertRedirects(response, f"/accounts/login/?next={self.url}")
 
-    def test_logged_in_response_status_code(self):
-        fake = Faker()
-        user_password = fake.password()
-        user = UserFactory(password=user_password)
+    def test_authenticated_user_response(self):
+        self.client.force_login(self.user)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 403)
 
-        self.client.login(email=user.email, password=user_password)
+    def test_authorized_user_response(self):
+        self.client.force_login(self.authorized_user)
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
+
+    def test_staff_user_response(self):
+        self.client.force_login(self.staff_user)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 403)
+
+    def test_template_used(self):
+        self.client.force_login(self.authorized_user)
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(response, "records/temperature_records_list.html")
+
+    def test_context_data_contains_temperature_records(self):
+        self.client.force_login(self.authorized_user)
+        response = self.client.get(self.url)
+        self.assertIn("temperature_records", response.context)
+
+    def test_is_paginated(self):
+        TemperatureRecordFactory.create_batch(11)
+        self.client.force_login(self.authorized_user)
+        response = self.client.get(self.url)
+        self.assertTrue(response.context.get("is_paginated"))
+        self.assertEqual(len(response.context.get("temperature_records")), 10)
+
+    def test_pagination_lists_all_items(self):
+        TemperatureRecordFactory.create_batch(12)
+        self.client.force_login(self.authorized_user)
+        response = self.client.get(self.url + "?page=2")
+        expected_records = list(TemperatureRecord.objects.all())[-2:]
+        people = list(response.context.get("temperature_records"))
+        self.assertEqual(people, expected_records)
+
+    def test_response_with_no_temperature_records(self):
+        self.client.force_login(self.authorized_user)
+        response = self.client.get(self.url)
+        with self.assertRaises(AssertionError):
+            self.assertInHTML(self.table_head, response.content.decode())
+        self.assertInHTML(
+            "There are no temperature records yet!", response.content.decode()
+        )
+
+    def test_response_with_temperature_records(self):
+        TemperatureRecordFactory.create_batch(3)
+        self.client.force_login(self.authorized_user)
+        response = self.client.get(self.url)
+        with self.assertRaises(AssertionError):
+            self.assertInHTML(
+                "There are no temperature records yet!", response.content.decode()
+            )
+        self.assertInHTML(self.table_head, response.content.decode())
--- records/views.py
+++ records/views.py
@@ -1,6 +1,12 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.views.generic import ListView
+
+from .models import TemperatureRecord
 
 
-class TemperatureRecordsListView(LoginRequiredMixin, TemplateView):
+class TemperatureRecordsListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
+    context_object_name = "temperature_records"
+    model = TemperatureRecord
+    paginate_by = 10
+    permission_required = "records.view_temperaturerecord"
     template_name = "records/temperature_records_list.html"
