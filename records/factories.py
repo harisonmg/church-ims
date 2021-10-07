4610333eb1e279123fe1c600415908256aec1937
Harison Gachuru <harisongachuru@gmail.com>
2021-10-07 23:10:07 +0300

    Update TemperatureRecordFactory with max and min from constants

--- records/factories.py
+++ records/factories.py
@@ -4,6 +4,7 @@ from factory.faker import Faker
 
 from people.factories import PersonFactory
 
+from . import constants
 from .models import TemperatureRecord
 
 
@@ -12,4 +13,9 @@ class TemperatureRecordFactory(DjangoModelFactory):
         model = TemperatureRecord
 
     person = SubFactory(PersonFactory)
-    body_temperature = Faker("pydecimal", right_digits=2, min_value=35, max_value=38)
+    body_temperature = Faker(
+        "pydecimal",
+        right_digits=2,
+        min_value=constants.MIN_HUMAN_BODY_TEMP,
+        max_value=constants.MAX_HUMAN_BODY_TEMP,
+    )
