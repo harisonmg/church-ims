3ec86c7d2f289c33c3444cd8f2eb9db10af92cd6
Harison Gachuru <harisongachuru@gmail.com>
2021-10-08 21:56:33 +0300

    Remove form validation tests from view

--- records/tests/test_forms.py
+++ records/tests/test_forms.py
@@ -1,6 +1,7 @@
 from django.test import SimpleTestCase
 from django.utils.module_loading import import_string
 
+from records import constants
 from records.forms import TemperatureRecordForm
 
 
@@ -18,18 +19,45 @@ class TemperatureRecordFormTestCase(SimpleTestCase):
         fields = self.form.fields.keys()
         self.assertEqual(["body_temperature"], list(fields))
 
-    def test_body_temperature_validators(self):
-        body_temperature_field = self.form.fields.get("body_temperature")
-        self.assertEqual(len(body_temperature_field.validators), 3)
+
+class TemperatureRecordBodyTemperatureTestCase(SimpleTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        cls.form = TemperatureRecordForm
+        cls.form_fields = cls.form().fields
+        cls.field = cls.form_fields.get("body_temperature")
+
+    def test_validators(self):
+        self.assertEqual(len(self.field.validators), 3)
         self.assertIsInstance(
-            body_temperature_field.validators[0],
+            self.field.validators[0],
             import_string("django.core.validators.MaxValueValidator"),
         )
         self.assertIsInstance(
-            body_temperature_field.validators[1],
+            self.field.validators[1],
             import_string("django.core.validators.MinValueValidator"),
         )
         self.assertIsInstance(
-            body_temperature_field.validators[2],
+            self.field.validators[2],
             import_string("django.core.validators.DecimalValidator"),
         )
+
+    def test_value_above_max(self):
+        data = {"body_temperature": constants.MAX_HUMAN_BODY_TEMP + 1}
+        form = self.form(data=data)
+        self.assertFalse(form.is_valid())
+        errors = {
+            "body_temperature": ["Ensure this value is less than or equal to 45."]
+        }
+        self.assertEqual(form.errors, errors)
+
+    def test_value_below_min(self):
+        data = {"body_temperature": constants.MIN_HUMAN_BODY_TEMP - 1}
+        form = self.form(data=data)
+        self.assertFalse(form.is_valid())
+        errors = {
+            "body_temperature": ["Ensure this value is greater than or equal to 30."]
+        }
+        self.assertEqual(form.errors, errors)
--- records/tests/test_views.py
+++ records/tests/test_views.py
@@ -5,7 +5,6 @@ from django.utils.module_loading import import_string
 
 from accounts.factories import UserFactory
 from people.factories import PersonFactory
-from records import constants
 from records.factories import TemperatureRecordFactory
 from records.models import TemperatureRecord
 
@@ -188,26 +187,9 @@ class TemperatureRecordCreateViewTestCase(TestCase):
         form = response.context.get("form")
         self.assertEqual(form.__class__.__name__, "TemperatureRecordForm")
         self.assertIsInstance(form, import_string("django.forms.ModelForm"))
-
-    def test_form_fields(self):
-        self.client.force_login(self.authorized_user)
-        response = self.client.get(self.url)
-        form = response.context.get("form")
-        self.assertEqual(list(form.fields.keys()), ["body_temperature"])
-
-    def test_form_with_body_temperature_above_max(self):
-        self.client.force_login(self.authorized_user)
-        data = {"body_temperature": constants.MAX_HUMAN_BODY_TEMP + 1}
-        response = self.client.post(self.url, data)
-        form = response.context.get("form")
-        self.assertFalse(form.is_valid())
-
-    def test_form_with_body_temperature_below_min(self):
-        self.client.force_login(self.authorized_user)
-        data = {"body_temperature": constants.MIN_HUMAN_BODY_TEMP - 1}
-        response = self.client.post(self.url, data)
-        form = response.context.get("form")
-        self.assertFalse(form.is_valid())
+        self.assertIsInstance(
+            form, import_string("records.forms.TemperatureRecordForm")
+        )
 
     def test_success_url(self):
         self.client.force_login(self.authorized_user)
