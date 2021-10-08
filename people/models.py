aa61579c0aa6cc4af87d04ba5e18b4f0c062abd5
Harison Gachuru <harisongachuru@gmail.com>
2021-10-08 16:08:05 +0300

    Add get_absolute_url to Person model

--- people/models.py
+++ people/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Person(models.Model):
@@ -13,3 +14,6 @@ class Person(models.Model):
 
     def __str__(self):
         return self.username
+
+    def get_absolute_url(self):
+        return reverse("people:person_detail", kwargs={"username": self.username})
