acb722256e6983f7f46c13f9784d2609a70a669f
Harison Gachuru <harisongachuru@gmail.com>
2021-10-08 15:53:06 +0300

    Add person update URL

--- people/tests/test_urls.py
+++ people/tests/test_urls.py
@@ -33,3 +33,14 @@ class PersonDetailURLTestCase(SimpleTestCase):
 
     def test_view_name(self):
         self.assertEqual(self.match.view_name, "people:person_detail")
+
+
+class PersonUpdateURLTestCase(SimpleTestCase):
+    def setUp(self):
+        self.match = resolve("/people/username/update/")
+
+    def test_view_func(self):
+        self.assertEqual(self.match.func.__name__, "PersonUpdateView")
+
+    def test_view_name(self):
+        self.assertEqual(self.match.view_name, "people:person_update")
--- people/urls.py
+++ people/urls.py
@@ -5,6 +5,9 @@ from . import views
 app_name = "people"
 urlpatterns = [
     path("add/", views.PersonCreateView.as_view(), name="person_create"),
+    path(
+        "<str:username>/update/", views.PersonUpdateView.as_view(), name="person_update"
+    ),
     path("<str:username>/", views.PersonDetailView.as_view(), name="person_detail"),
     path("", views.PeopleListView.as_view(), name="people_list"),
 ]
