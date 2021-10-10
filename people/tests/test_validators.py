23694f7e3bd38c9b18ab5a34d379e9aef0d8ce65
Harison Gachuru <harisongachuru@gmail.com>
2021-10-10 07:55:14 +0300

    Adda date of birth validator

--- people/tests/test_validators.py
+++ people/tests/test_validators.py
@@ -1,12 +1,14 @@
+from datetime import date, timedelta
 from unittest import TestCase
 
 from django.core.exceptions import ValidationError
 
 from people import validators
+from people.constants import MAX_HUMAN_AGE
 from people.factories import PersonFactory
 
 
-class FullNameValidatorTestCase(TestCase):
+class ValidateFullNameTestCase(TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
@@ -30,3 +32,18 @@ class FullNameValidatorTestCase(TestCase):
             ValidationError, validators.INVALID_FULL_NAME_ERROR
         ):
             validators.validate_full_name(f"{self.person.username} ")
+
+
+class ValidateDateOfBirthTestCase(TestCase):
+    def test_date_in_future(self):
+        with self.assertRaisesRegex(ValidationError, validators.DOB_IN_FUTURE_ERROR):
+            tomorrow = date.today() + timedelta(days=1)
+            validators.validate_date_of_birth(tomorrow)
+
+    def test_date_in_distant_past(self):
+        with self.assertRaisesRegex(
+            ValidationError, validators.DOB_IN_DISTANT_PAST_ERROR
+        ):
+            days_lived = 365.25 * (MAX_HUMAN_AGE + 1)
+            long_ago = date.today() - timedelta(days=round(days_lived))
+            validators.validate_date_of_birth(long_ago)
