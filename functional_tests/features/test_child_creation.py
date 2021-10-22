5096f157295b3cc508e9c8767a1b26fcfe891c1b
Harison Gachuru <harisongachuru@gmail.com>
2021-10-22 10:18:49 +0300

    Update child creation FT

--- functional_tests/features/test_child_creation.py
+++ functional_tests/features/test_child_creation.py
@@ -40,7 +40,7 @@ class ChildCreationTestCase(FunctionalTestCase):
         self.assertEqual(child_creation_page.form.submit_button_label, "Add")
 
         # He enters the child's username and full name and submits the form
-        child_creation_page.add_child(
+        child_creation_page.add_person(
             self.child.username,
             self.child.full_name,
             self.child.get_gender_display(),
@@ -58,6 +58,44 @@ class ChildCreationTestCase(FunctionalTestCase):
         )
         people = f"{self.parent.username} and {self.child.username}"
         self.assertEqual(
-            dashboard.messages[0],
+            dashboard.messages[1],
             f"The relationship between {people} has been added successfully.",
+        )
+
+    def test_child_creation_by_non_parent(self):
+        # A user visits the child creation page.
+        child_creation_page = pages.ChildCreationPage(self)
+        child_creation_page.visit()
+
+        # He knows he's in the right place because he can see the name
+        # of the site in the title and header
+        self.assertEqual(child_creation_page.title, self.SITE_NAME)
+        self.assertEqual(child_creation_page.header.title, self.header_title)
+        self.assertEqual(child_creation_page.heading, "Add a child's information")
+
+        # He sees the inputs of the child form, including labels and placeholders.
+        self.assertEqual(child_creation_page.form.username_label, "Username*")
+        self.assertEqual(child_creation_page.form.full_name_label, "Full name*")
+        self.assertEqual(child_creation_page.form.gender_label, "Gender*")
+        self.assertEqual(child_creation_page.form.date_of_birth_label, "Date of birth*")
+        self.assertEqual(
+            child_creation_page.form.is_parent_checkbox_label, "I am the child's parent"
+        )
+        self.assertEqual(child_creation_page.form.submit_button_label, "Add")
+
+        # He enters the child's username and full name and submits the form
+        child_creation_page.add_person(
+            self.child.username,
+            self.child.full_name,
+            self.child.get_gender_display(),
+            str(self.child.dob),
+        )
+
+        # The child's information was added successfully and he is redirected
+        # to the his dashboard
+        dashboard = pages.Dashboard(self)
+        self.assertEqual(self.browser.current_url, dashboard.url)
+        self.assertEqual(
+            dashboard.messages[0],
+            f"{self.child.username}'s information has been added successfully.",
         )
