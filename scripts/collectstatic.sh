ccf688678740782b842aa444150cf888023367c8
Harison Gachuru <harisongachuru@gmail.com>
2021-10-25 13:07:54 +0300

    Fix bug in the collectstatic script

--- scripts/collectstatic.sh
+++ scripts/collectstatic.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-DJANGO_SETTINGS_MODULE=config.settings.production
 export STATIC_FILES_DIR=gs://"$GCP_STORAGE_BUCKET_NAME"/static
 
 # Collect the static files
 echo "Collecting static files"
-python manage.py collectstatic --no-input
+pipenv run python manage.py collectstatic --no-input \
+    --settings=config.settings.production
 
 # Synchronize the content of the source bucket with other buckets
 IFS=',' read -r -a storage_buckets <<< "$DESTINATION_STORAGE_BUCKET_NAMES"
