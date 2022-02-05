213d5d34b081321b8dded56afeffe7cfcbc021f9
Harison Gachuru <harisongachuru@gmail.com>
2022-02-05 10:15:25 +0300

    Add methods to handle each field independently in auth forms

--- functional_tests/pages/components/forms/auth.py
+++ functional_tests/pages/components/forms/auth.py
@@ -17,6 +17,12 @@ class EmailComponent(BaseComponent):
     def email_label(self):
         return self.browser.find_element(*self.EMAIL_LABEL).text
 
+    def clear_email_input(self):
+        self._email_input.clear()
+
+    def enter_email(self, email):
+        self._email_input.send_keys(email)
+
 
 class UsernameOrEmailComponent(EmailComponent):
     EMAIL_INPUT = (By.CSS_SELECTOR, "input#id_login")
@@ -35,6 +41,12 @@ class PasswordComponent(BaseComponent):
     def password_label(self):
         return self.browser.find_element(*self.PASSWORD_LABEL).text
 
+    def clear_password_input(self):
+        self._password_input.clear()
+
+    def enter_password(self, password):
+        self._password_input.send_keys(password)
+
 
 class PasswordConfirmationComponent(PasswordComponent):
     PASSWORD_INPUT = (By.CSS_SELECTOR, "input#id_password1")
@@ -49,6 +61,12 @@ class PasswordConfirmationComponent(PasswordComponent):
     @property
     def password_confirmation_label(self):
         return self.browser.find_element(*self.PASSWORD_CONFIRMATION_LABEL).text
+
+    def clear_password2_input(self):
+        self._password_confirmation_input.clear()
+
+    def enter_password2(self, password):
+        self._password_confirmation_input.send_keys(password)
 
 
 # forms
@@ -75,6 +93,9 @@ class LoginForm(UsernameOrEmailComponent, PasswordComponent, SubmitFormComponent
     def password_reset_link(self):
         element = self.browser.find_element(*self.PASSWORD_RESET_LINK)
         return {element.text: element.get_attribute("href")}
+
+    def click_remember_checkbox(self):
+        self._remember_checkbox.click()
 
     def send_keys(self, email, password, remember=False):
         self._email_input.send_keys(email)
